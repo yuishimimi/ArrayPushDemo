249fd9164313ef573929add356ec9abe5248268c
yuishimimi <42453458+yuishimimi@users.noreply.github.com>
2018-08-16 13:45:58 -1000

    Update
    I added comments to my code because Mr. Kam will rage hard if I did not.

--- arraypushdemo.js
+++ arraypushdemo.js
@@ -1,8 +1,8 @@
-var firstName = [];
-var lastName = [];
+var firstName = [];  //Start with empty array
+var lastName = [];  //start with empty array
 
 var userFirst = prompt("What's your first name");
-firstName.push(userFirst);
+firstName.push(userFirst);  //Pushes to the end of the array
 var userLast = prompt("what's your last name");
 lastName.push(userLast);
 window.alert(firstName[0] + lastName[0] + firstName.length + lastName.length);
